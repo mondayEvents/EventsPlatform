d8f12c7adfa3692ce4c251448e3e82c66d56373b
Pablo <pabllobeg@hotmail.com>
2017-08-09 21:44:29 -0300

    Added the Models for EventManager, EventPlace and Event

---
+++ src/Model/Table/EventManagersTable.php
@@ -0,0 +1,84 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * EventManagers Model
+ *
+ * @property \App\Model\Table\EventsTable|\Cake\ORM\Association\BelongsTo $Events
+ * @property \App\Model\Table\UsersTable|\Cake\ORM\Association\BelongsTo $Users
+ *
+ * @method \App\Model\Entity\EventManager get($primaryKey, $options = [])
+ * @method \App\Model\Entity\EventManager newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\EventManager[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\EventManager|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\EventManager patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\EventManager[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\EventManager findOrCreate($search, callable $callback = null, $options = [])
+ */
+class EventManagersTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('event_managers');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Events', [
+            'foreignKey' => 'event_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->uuid('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->boolean('is_active')
+            ->requirePresence('is_active', 'create')
+            ->notEmpty('is_active');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['event_id'], 'Events'));
+        $rules->add($rules->existsIn(['users_id'], 'Users'));
+
+        return $rules;
+    }
+}
---
+++ src/Model/Table/EventPlacesTable.php
@@ -0,0 +1,86 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * EventPlaces Model
+ *
+ * @property \App\Model\Table\EventsTable|\Cake\ORM\Association\BelongsTo $Events
+ *
+ * @method \App\Model\Entity\EventPlace get($primaryKey, $options = [])
+ * @method \App\Model\Entity\EventPlace newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\EventPlace[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\EventPlace|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\EventPlace patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\EventPlace[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\EventPlace findOrCreate($search, callable $callback = null, $options = [])
+ */
+class EventPlacesTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('event_places');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Events', [
+            'foreignKey' => 'events_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->uuid('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->allowEmpty('ltd');
+
+        $validator
+            ->allowEmpty('lat');
+
+        $validator
+            ->allowEmpty('lng');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['events_id'], 'Events'));
+
+        return $rules;
+    }
+}
