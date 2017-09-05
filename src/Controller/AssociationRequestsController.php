e7329cea2ef51911ccfd59f8ac5221e4ef4b8e68
Pablo Henrique <pabllobeg@gmail.com>
2017-09-05 13:57:17 -0300

    Decline Association Request and Request association methods

--- src/Controller/AssociationRequestsController.php
+++ src/Controller/AssociationRequestsController.php
@@ -28,7 +28,7 @@ class AssociationRequestsController extends AppController
         $this->setResponseMessage(compact('pendingAssociations'));
         $this->buildResponse();
     }
-    
+
     /*
     * Accept pending association request
     *
@@ -67,85 +67,63 @@ class AssociationRequestsController extends AppController
        $this->buildResponse();
    }
 
+
     /**
-     * View method
+     * Decline pending association request
      *
-     * @param string|null $id Association Request id.
+     * @param string|null $request_id Association Request id.
      * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
      */
-    public function view($id = null)
-    {
-        $associationRequest = $this->AssociationRequests->get($id, [
-            'contain' => ['Users', 'Events', 'Parent']
-        ]);
-
-        $this->set('associationRequest', $associationRequest);
-        $this->set('_serialize', ['associationRequest']);
-    }
-
-    /**
-     * @param string $asked_event_id
-     */
-    function requestAssociation ($asked_event_id = null)
-    {
-        $this->request->allowMethod(['post']);
-
-        $events = $this->AssociationRequests->Events
-            ->find()
-            ->where(['Events.id IN' => [$asked_event_id, $this->request->getData('event_id')]])
-            ->limit(2)
-            ->toArray();
-
-        if (count($events) < 2) {
-            $error = ['message' => __('One of the events could not be found. Please, verify the your IDs')];
-            $this->response(400, compact('error'));
-            return;
-        }
-
-        $index = (array_column($events, 'id')[0] == $asked_event_id) ? 0 : 1;
-        $event_asked = $events[$index];
-        $event_asker = $events[!$index];
-
-        $ownsBoth = $this->AssociationRequests->Events
-            ->ownershipChecker($events);
-
-        if ($ownsBoth)
-        {
-            $wasSaved = $this->AssociationRequests->Events->AdditionalEvents
-                ->associateEvents($event_asked, $event_asker);
-
-            if (!$wasSaved) {
-                $error = $wasSaved;
-                $this->response(400, compact('error'));
-                return;
-            }
-
-            $message = ['_created' => __('Your events are now related')];
-            $this->response(200, compact('message'));
-            return;
-        }
-
-        $data = ['active' => true, 'message' => $this->request->getData('message')];
-        $associationRequest = $this->AssociationRequests->newEntity($data);
-
-        $associationRequest->user = $this->AssociationRequests->Users
-            ->get($this->Auth->user('uid'), ['contain'=>[]]);
-
-        $associationRequest->event = $event_asker;
-        $associationRequest->event_parent = $event_asked;
-
-        $saved = $this->AssociationRequests->save($associationRequest, ['associated' => [
-            'Users', 'Events', 'EventParents']
-        ]);
-
-        if (!$saved) {
-            $error = $associationRequest->getErrors();
-            $this->response(400, compact('error'));
-            return;
-        }
-
-        $message = ['_sent' => __('Your association request is waiting for approval!')];
-        $this->response(200, compact('message'));
-    }
+     public function decline($request_id = null)
+     {
+         $this->request->allowMethod(['get']);
+ 
+         try {
+             $request = $this->AssociationRequests->get($request_id);
+             $request->declineEventAssociation();
+             $this->AssociationRequests->saveOrFail($request);
+ 
+         } catch (PersistenceFailedException $exception) {
+             $this->setResponseCode(406);
+             $this->setResponseMessage(['error' => $exception->getEntity()->getErrors()]);
+ 
+         } catch (\Exception $exception) {
+             $this->setResponseCode(500);
+             $this->setResponseMessage(['message' => ['_error' => $exception->getMessage()]]);
+         }
+ 
+         $this->buildResponse();
+     }
+ 
+     /**
+      * @param string $asked_event_id
+      */
+     function requestAssociation ($asked_event_id = null)
+     {
+         $this->request->allowMethod(['post']);
+ 
+         try {
+             $user = $this->AssociationRequests->Users->get($this->Auth->user('uid'));
+             $sub_event = $this->AssociationRequests->Events->get($this->request->getData('event_id'), ['contain' => ['Users']]);
+             $event = $this->AssociationRequests->Events->get($asked_event_id, ['contain' => ['Users']]);
+ 
+             $request = $this->AssociationRequests->newEntity();
+ 
+             $request->setAssociationRequest($user, $event, $sub_event, $this->request->getData('event_id'));
+             $this->AssociationRequests->saveOrFail($request);
+ 
+             $this->setResponseMessage(['message' => ['_success' => __("Your request was sent!")]]);
+ 
+         } catch (PersistenceFailedException $exception) {
+             $this->setResponseCode(406);
+             $this->setResponseMessage(['error' => $exception->getEntity()->getErrors()]);
+ 
+         } catch (\Exception $exception) {
+             $this->setResponseCode(500);
+             $this->setResponseMessage(['message' => ['_error' => $exception->getMessage()]]);
+         }
+ 
+         $this->buildResponse();
+     }
+   
 }
