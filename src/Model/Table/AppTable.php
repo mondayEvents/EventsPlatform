fb590997631b671aae8193d214d347651ee0a470
Pablo Henrique <pabllobeg@gmail.com>
2017-09-05 14:51:49 -0300

    ApTable changes

--- src/Model/Table/AppTable.php
+++ src/Model/Table/AppTable.php
@@ -1,23 +1,108 @@
 <?php
 namespace App\Model\Table;
 
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
+use Cake\Validation\Validator;
+use Cake\Database\Schema\TableSchema;
 
 /**
- * Application Table
+ * AssociationRequests Model
  *
- * Application-wide methods are implemented here,
- * so Table Models can inherit them.
+ * @property \App\Model\Table\UsersTable|\Cake\ORM\Association\BelongsTo $Users
+ * @property \App\Model\Table\EventsTable|\Cake\ORM\Association\BelongsTo $Events
+ * @property |\Cake\ORM\Association\BelongsTo $EventParents
+ *
+ * @method \App\Model\Entity\AssociationRequest get($primaryKey, $options = [])
+ * @method \App\Model\Entity\AssociationRequest newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\AssociationRequest[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\AssociationRequest|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\AssociationRequest patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\AssociationRequest[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\AssociationRequest findOrCreate($search, callable $callback = null, $options = [])
  */
-abstract class AppTable extends Table
+class AssociationRequestsTable extends Table
 {
-    public function beforeFind($event, $query, $options, $primary)
+
+    /**
+     * Initialize Schema method
+     *
+     * @param TableSchema $schema
+     * @return TableSchema
+     */
+    protected function _initializeSchema(TableSchema $schema)
     {
-        if(!isset($options['active'])){
-            $query->where([
-                $event->subject()->alias() . '.active' => 1
-            ]);
-        }
-        return $query;
+        $schema->columnType('status', 'AssociationRequestType');
+        return $schema;
+    }
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('association_requests');
+        $this->setDisplayField('id');
+        $this->setPrimaryKey('id');
+
+        $this->belongsTo('Users', [
+            'foreignKey' => 'user_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('Events', [
+            'foreignKey' => 'event_id',
+            'joinType' => 'INNER'
+        ]);
+        $this->belongsTo('EventParents', [
+            'className' => 'Events',
+            'foreignKey' => 'event_parent_id',
+            'joinType' => 'INNER'
+        ]);
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->uuid('id')
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('message', 'create')
+            ->notEmpty('message');
+
+        $validator
+            ->boolean('active')
+            ->requirePresence('active', 'create')
+            ->notEmpty('active');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->existsIn(['user_id'], 'Users'));
+        $rules->add($rules->existsIn(['event_id'], 'Events'));
+        $rules->add($rules->existsIn(['event_parent_id'], 'EventParents'));
+
+        return $rules;
     }
 }
